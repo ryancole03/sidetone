7c813ba390dc69d56472f9c52cb35417dd20789c
Ryan Cole <ryancole03@users.noreply.github.com>
2026-04-17 05:53:46 +0930

    Improve build.bat to auto-detect SDK versions and copy DLL

--- app/build.bat
+++ app/build.bat
@@ -3,35 +3,43 @@ setlocal
 
 echo Setting up build environment...
 
-set MSVC_DIR=C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools
+set VCPKG_DIR=%~dp0..\..\vcpkg\installed\x64-windows
+set MSVC_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2022
 set SDK_DIR=C:\Program Files (x86)\Windows Kits\10
-set VCPKG_DIR=C:\Users\Ryan\vcpkg\installed\x64-windows
 
-set CL_PATH=%MSVC_DIR%\VC\Tools\MSVC\14.50.35717\bin\Hostx64\x64
-set INCLUDE=%MSVC_DIR%\VC\Tools\MSVC\14.50.35717\include
+for /d %%D in ("%MSVC_DIR%\*\BuildTools\VC\Tools\MSVC\*") do set MSVC_VER=%%D
+for /d %%D in ("%SDK_DIR%\Include\10.*") do set SDK_VER=%%~nxD
+
+set CL_PATH=%MSVC_VER%\bin\Hostx64\x64
+set INCLUDE=%MSVC_VER%\include
 set INCLUDE=%INCLUDE%;%VCPKG_DIR%\include
-set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\10.0.26100.0\ucrt
-set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\10.0.26100.0\shared
-set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\10.0.26100.0\um
-set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\10.0.26100.0\winrt
+set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\%SDK_VER%\ucrt
+set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\%SDK_VER%\shared
+set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\%SDK_VER%\um
+set INCLUDE=%INCLUDE%;%SDK_DIR%\Include\%SDK_VER%\winrt
 
-set LIB=%MSVC_DIR%\VC\Tools\MSVC\14.50.35717\lib\onecore\x64
+set LIB=%MSVC_VER%\lib\onecore\x64
 set LIB=%LIB%;%VCPKG_DIR%\lib
-set LIB=%LIB%;%SDK_DIR%\Lib\10.0.26100.0\ucrt\x64
-set LIB=%LIB%;%SDK_DIR%\Lib\10.0.26100.0\um\x64
+set LIB=%LIB%;%SDK_DIR%\Lib\%SDK_VER%\ucrt\x64
+set LIB=%LIB%;%SDK_DIR%\Lib\%SDK_VER%\um\x64
 
 set PATH=%CL_PATH%;%PATH%
 
+if not exist "%~dp0..\libspeexdsp.dll" (
+    echo Copying libspeexdsp.dll...
+    copy "%VCPKG_DIR%\bin\libspeexdsp.dll" "%~dp0..\libspeexdsp.dll"
+)
+
 echo Building native library...
 cl ..\engine.c /LD /O2 /D_UNICODE /DNATIVELIB_EXPORTS ^
- /I"%MSVC_DIR%\VC\Tools\MSVC\14.50.35717\include" ^
+ /I"%MSVC_VER%\include" ^
  /I"%VCPKG_DIR%\include" ^
- /I"%SDK_DIR%\Include\10.0.26100.0\ucrt" ^
- /I"%SDK_DIR%\Include\10.0.26100.0\shared" ^
- /I"%SDK_DIR%\Include\10.0.26100.0\um" ^
- /I"%SDK_DIR%\Include\10.0.26100.0\winrt" ^
+ /I"%SDK_DIR%\Include\%SDK_VER%\ucrt" ^
+ /I"%SDK_DIR%\Include\%SDK_VER%\shared" ^
+ /I"%SDK_DIR%\Include\%SDK_VER%\um" ^
+ /I"%SDK_DIR%\Include\%SDK_VER%\winrt" ^
  /Fe:NativeLib.dll ^
- /link /LIBPATH:"%MSVC_DIR%\VC\Tools\MSVC\14.50.35717\lib\onecore\x64" /LIBPATH:"%VCPKG_DIR%\lib" /LIBPATH:"%SDK_DIR%\Lib\10.0.26100.0\ucrt\x64" /LIBPATH:"%SDK_DIR%\Lib\10.0.26100.0\um\x64" ^
+ /link /LIBPATH:"%MSVC_VER%\lib\onecore\x64" /LIBPATH:"%VCPKG_DIR%\lib" /LIBPATH:"%SDK_DIR%\Lib\%SDK_VER%\ucrt\x64" /LIBPATH:"%SDK_DIR%\Lib\%SDK_VER%\um\x64" ^
  speexdsp.lib user32.lib gdi32.lib comctl32.lib ole32.lib uuid.lib mmdevapi.lib shell32.lib uxtheme.lib dwmapi.lib
 
 if %errorlevel% neq 0 (
@@ -41,11 +49,11 @@ if %errorlevel% neq 0 (
 )
 
 echo.
-echo Build SUCCESS - NativeLib.dll
+echo NativeLib.dll built successfully.
 
 echo.
-echo Building C# app...
-dotnet build "%~dp0SidetoneApp.csproj" -c Release
+echo Publishing self-contained app...
+dotnet publish "%~dp0SidetoneApp.csproj" -c Release -o "%~dp0publish"
 if %errorlevel% neq 0 (
     echo Build FAILED
     pause
@@ -53,4 +61,14 @@ if %errorlevel% neq 0 (
 )
 
 echo.
-echo Build SUCCESS
+echo Copying NativeLib.dll to publish folder...
+copy /Y "%~dp0NativeLib.dll" "%~dp0publish\"
+copy /Y "..\libspeexdsp.dll" "%~dp0publish\"
+
+echo.
+echo Build SUCCESS - Portable app in app\publish\
+echo.
+echo Files needed:
+echo   - Sidetone.exe
+echo   - NativeLib.dll
+echo   - libspeexdsp.dll
